47ddb4f2ea16ceb90437b98bbb4a01ebb51eb662
AchyutaAich <send2aich@gmail.com>
2017-08-20 11:50:18 +0530

    Added feature for finding the nth entry in the list

--- List.cpp
+++ List.cpp
@@ -18,15 +18,28 @@ struct length{
   const static int result = length<typename list::tail>::result + 1;
 };
 
-
 template <>
 struct length<NIL>{
   const static int result = 0;
 };
 
 
+template <typename list, int n>
+struct Nth {
+  static const int result = Nth<typename list::tail,n-1>::result;
+};
+
+template <typename list>
+struct Nth<list,1>{
+  static const int result = list::head::result;
+};
+
+
+
+
 int main(){
   using list123 = list< Int<1>, list< Int<2>, list<Int<3> > > >;
   std::cout << length<list123>::result << std::endl;
+  std::cout << Nth<list123,1>::result << " " << Nth<list123,2>::result << " " << Nth<list123,3>::result << std::endl;
   return 0;
 }
